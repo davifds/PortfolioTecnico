728a59a34906ec2a379e23c556e3432d0de0ab5f
DAVI ERTHAL MARTINS <davi_e_martins@estudante.sesisenai.org.br>
2022-03-31 09:15:34 -0300

    Adicionando informações sobre o meu README

---
+++ LogicaComputacional/AtividadePA.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int a1 = 15;
+        int n = 20;
+        int r = 17;
+        int an = a1 + (n-1) * r:
+        System.out.println(an);
+        a1 = an - (n-1) *r;
+        System.out.printlb(a1);
+        r = (an - a1)/(n - 1);
+        System.out.println(r);
+        //r = (an - a1)/(n - 1)
+        //(n-1) = (an -a1)/r
+        //n = ((an - a1)/r) +1
+        n = ((an - a1)/r) + 1;
+        System.out.println(n);
+     }
+}
