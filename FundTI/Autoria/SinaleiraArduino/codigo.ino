e890b8b11ddffc52f91a9498da98d291c25d2897
Davi <102593153+davifds@users.noreply.github.com>
2022-06-30 08:09:46 -0300

    Update codigo.ino

--- FundTI/Autoria/SinaleiraArduino/codigo.ino
+++ FundTI/Autoria/SinaleiraArduino/codigo.ino
@@ -1,1 +1,26 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(8, OUTPUT);
+}
 
+void loop()
+{
+  digitalWrite(8, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(8, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+
+ 
+  digitalWrite(7, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+digitalWrite(9, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  }
